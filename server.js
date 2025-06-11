e781e8a50901355c4f8b8f63e3579d1181fa263a
la-compagna-vertrag <ricogbzela@gmail.com>
2025-06-11 21:22:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
